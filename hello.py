4e6b261beb6cf651ea322b4c59b232233d10ae28
Nur Jannah <2603419@sit.singaporetech.edu.sg>
2026-09-04 14:49:34 +0800

    Activity 4

--- hello.py
+++ hello.py
@@ -1,22 +1,31 @@
+#print ("==================")
+#print("Welcome here")
+#print("My firts post!")
+#print ("==================")
+
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Category: ")
+
+print("\n Instagram Profile")
 print ("==================")
-print("Welcome here")
-print("My firts post!")
-print ("==================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
 
+# bio = "Fun Blogger"
+# followers = 100
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
+# followers += 50 
+# #print("Day 1: ",followers) 
 
-followers += 50 
-print("Day 1: ",followers) 
+# followers += 20 
+# print("Day 2: ",followers) 
 
-followers += 20 
-print("Day 2: ",followers) 
-
-followers -= 10 
-print("Day 3: ",followers)
+# followers -= 10 
+# print("Day 3: ",followers)*\
 
 print("Username:", username)
-print("Bio:", bio) 
-print("Followers:", followers)
+#print("Bio:", bio) 
+#print("Followers:", followers)
