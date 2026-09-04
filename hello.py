32e5d582dff262103790168be66f0da5af377520
Nur Jannah <2603419@sit.singaporetech.edu.sg>
2026-09-04 14:41:11 +0800

    Activity 2

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print ("==================")
 print("Welcome here")
 print("My firts post!")
 print ("==================")
+
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio) 
+print("Followers:", followers)
