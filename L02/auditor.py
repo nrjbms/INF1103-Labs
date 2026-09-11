0499df9df4383b6030e75b849cbf56ee18aa0279
Nur Jannah <2603419@sit.singaporetech.edu.sg>
2026-09-11 10:59:44 +0800

    Edited auditor.py

---
+++ L02/auditor.py
@@ -0,0 +1,33 @@
+
+inventory = 0
+failed = 0
+
+while True:
+
+    user_input = input("Enter a number (or 'quit' to exit): ")
+    if user_input.lower() == 'quit':
+        break
+
+    if user_input.isdigit() == False:
+        print("Invalid input. Please enter a valid number.")
+        failed += 1
+        continue
+
+    if int(user_input) < 0 :
+        print("Invalid input. Please enter a non-negative number.")
+        failed += 1
+        continue
+
+    inventory += int(user_input)
+
+    if inventory >= 500:
+                print("Inventory limit exceeded.")
+                failed += 1
+                break
+    
+    
+
+
+
+print("Total Units Processed: ", inventory)
+print("Number of Failed/Rejected Entries: ",failed)
